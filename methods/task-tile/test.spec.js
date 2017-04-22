c501fa067e5eab0bf278fcb1349371ed69126b2d
yuchiko <iyuchiko@gmail.com>
2017-04-22 15:53:41 +0300

    change project structure

--- methods/task-tile/test.spec.js
+++ methods/task-tile/test.spec.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const algolib = require('../index.js');
+const coverByTile = require('./index.js');
 
 describe('#coverByTile', function() {
 
   it('should count amount of tiles to cover area (integer)', function() {
-    let tilesAmount = algolib.coverByTile(5, 3, 2);
+    let tilesAmount = coverByTile(5, 3, 2);
 
     tilesAmount.should.be.eql(6);
   });
 
   it('should count amount of tiles to cover area (float)', function() {
-    let tilesAmount = algolib.coverByTile(0.5, 1.3, 0.2);
+    let tilesAmount = coverByTile(0.5, 1.3, 0.2);
 
     tilesAmount.should.be.eql(21);
   });
